9a594ad6c5789e8075ab4087460ddcf62004167b
kds98 <skyudae@naver.com>
2023-10-30 09:30:55 +0900

    개발환경 세팅

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,15 @@
+package com.ll;
+
+public class Calc {
+    public void run() {
+
+    }
+
+    public int multiply (int a, int b) {
+        return a * b;
+    }
+
+    public int plus (int a, int b) {
+        return a + b;
+    }
+}
